d0e47efe03120fe819b04cbb4ab1b4a2ddebcb03
prabhsembhi <prabhpreetsingh013@gmail.com>
2020-06-20 10:46:53 -0700

    Included the index.html and the post method

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function (req, res) {
+
+  res.sendFile(__dirname + "/index.html");
+ 
+});
+
+app.post("/", function(req,res){
+  const query = req.body.cityName;
+  const api = "7ec7c4611d5e73a9b3d2060c9261f631";
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=moga&appid=7ec7c4611d5e73a9b3d2060c9261f631&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+api+"&units=metric";
   https.get(url, function (response) {
     console.log(response.statusCode);
 
@@ -16,13 +27,13 @@ app.get("/", function (req, res) {
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
 
-      res.write("<h1>The Temperature for moga is"+temp+"</h1>");
+      res.write("<h1>The Temperature for "+query+" is "+temp+"</h1>");
       res.write("<p>The weather condition is like"+weatherDescription+"</p>")
       res.write("<img src=" + icon + ">")
       res.send();
     });
   });
-});
+})
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000.");
